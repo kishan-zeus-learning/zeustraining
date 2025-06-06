c082d7557d6a8fe0f51f7431cff736fbb7c51c4d
Kishan Kumar Vadhia <kishan.vadhia@zeuslearning.com>
2025-06-06 11:32:40 +0530

    Fix task1 task2 bugs

--- task1/script.js
+++ task1/script.js
@@ -4,36 +4,36 @@ const genderMale=document.getElementById("male");
 const genderFemale=document.getElementById("female");
 const btn=document.getElementById("submit");
 
-btn.addEventListener("click",(event)=>{
-    event.preventDefault();
-    if(!fullName.value){
-        alert("please enter name");
-        return ;
-    }
-    if(!comments.value){
-        alert("please enter comments");
-        return ;
-    }
+// btn.addEventListener("click",(event)=>{
+//     event.preventDefault();
+//     if(!fullName.value){
+//         alert("please enter name");
+//         return ;
+//     }
+//     if(!comments.value){
+//         alert("please enter comments");
+//         return ;
+//     }
 
-    if(genderMale.checked && genderFemale.checked){
-        alert("Can't enter 2 genders");
-        return ;
-    }
+//     if(genderMale.checked && genderFemale.checked){
+//         alert("Can't enter 2 genders");
+//         return ;
+//     }
 
-    if(!genderMale.checked && !genderFemale.checked){
-        alert("Please enter gender");
-        return ;
-    }
+//     if(!genderMale.checked && !genderFemale.checked){
+//         alert("Please enter gender");
+//         return ;
+//     }
 
-    console.log("full name: ",fullName.value);
-    console.log("comments: ",comments.value);
-    if(genderFemale.checked){
-        console.log("gender: Female");
-    }
+//     console.log("full name: ",fullName.value);
+//     console.log("comments: ",comments.value);
+//     if(genderFemale.checked){
+//         console.log("gender: Female");
+//     }
 
-    if(genderMale.checked){
-        console.log("gender: Male");
-    }
+//     if(genderMale.checked){
+//         console.log("gender: Male");
+//     }
 
 
-});
+// });
